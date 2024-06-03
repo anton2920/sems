6dbd986b20f5c9c33b9fd7103e52380554c56e18
anton2920 <anton2920@gmail.com>
2024-06-03 18:18:31 +0300

    Added test for subject create page handler

---
+++ subject_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/anton2920/gofa/net/http"
+	"github.com/anton2920/gofa/net/url"
+)
+
+func TestSubjectCreatePageHandler(t *testing.T) {
+	const endpoint = "/subject/create"
+
+	expectedOK := [...]url.Values{
+		{},
+		{{"GroupID", []string{"0", "a"}}, {"TeacherID", []string{"0", "a"}}},
+	}
+
+	for _, test := range expectedOK {
+		testPostAuth(t, endpoint, testTokens[AdminID], test, http.StatusOK)
+	}
+
+	testPostInvalidFormAuth(t, endpoint, testTokens[AdminID])
+
+	testPost(t, endpoint, nil, http.StatusUnauthorized)
+	testPostAuth(t, endpoint, testInvalidToken, nil, http.StatusUnauthorized)
+
+	testPostAuth(t, endpoint, testTokens[1], nil, http.StatusForbidden)
+}
